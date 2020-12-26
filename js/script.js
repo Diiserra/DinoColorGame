2c046e02ae7f04602b0eb94ebd51bf504e0bd9e2
Diego Lima <diiserras@gmail.com>
2020-12-26 16:58:10 -0300

    Adicionando a função pulo a tecla espaço do teclado e algumas melhorias no código

--- js/script.js
+++ js/script.js
@@ -1,24 +1,57 @@
 //Controler variable
 let positionX = 1;
 let positionY = 1;
-let diretionX = 0;
-let diretionY = 0;
-let velocity;
+let diretionX = 1010;
+let diretionY = 410;
+let velocityGame = 5;
+let velocityJump = 7;
 let animation;
+let anigravity;
 
 function render(){
-    velocity = 0.5;
-    gravity(positionY, velocity)
-    animation = requestAnimationFrame(render)
+    gravity() 
+    moveFloor()
 }
 
-function gravity(positionY,  velocity){
-    let pY = positionY;
-    let vel = velocity;
-    if (diretionY < 70){
-        diretionY+= pY * vel;
-        console.log(diretionY);
+render
+
+
+
+window.addEventListener('keydown', (key) => {
+    if (key.keyCode === 32 && diretionY === 410) {
+        jump()
+    }
+})
+
+
+function gravity(){
+    if (diretionY < 410){
+        diretionY+= positionY * velocityJump;
         let player = document.getElementById('player');
-        player.style.top = diretionY +'%'
-    }    
+        player.style.top = diretionY +'px'
+        console.log(diretionY);
+        console.log(diretionY);
+        anigravity = requestAnimationFrame(gravity)
+    }
+}
+
+function jump(){
+    if (diretionY > 240){
+        diretionY-= positionY * velocityJump;
+        let player = document.getElementById('player');
+        player.style.top = diretionY +'px'
+        animation = requestAnimationFrame(jump)
+    }else{
+        gravity()
+    }
+}
+
+function moveFloor(){
+    diretionX-= positionX + velocityGame;
+    let cactus = document.getElementById('cactus');
+    let floor = document.getElementById('floor');
+    cactus.style.left = diretionX + 'px'
+    floor.style.left = diretionX + 'px'
+    console.log(diretionX);
+    animation = requestAnimationFrame(moveFloor)
 }
